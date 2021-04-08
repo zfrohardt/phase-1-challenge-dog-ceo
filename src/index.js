0e372b00ccbc89465d7d4d457cf89b91b01e1e43
Zach Frohardt <zfrohardt@gmail.com>
2021-04-08 15:31:08 -0700

    completed challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", event => {
+    randomDogImages();
+})
+
+let randomDogImages = () => {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(response => response.json())
+    .then(json => {
+        let imageContainer = document.getElementById("dog-image-container");
+        for(imageURL of json.message) {
+            let img = document.createElement("img");
+            img.src = imageURL;
+            imageContainer.appendChild(img);
+        }
+    })
+}
