746da0501a9dc33947c22d53cd64ab07358dde8e
Zach Frohardt <zfrohardt@gmail.com>
2021-04-08 15:42:40 -0700

    completed challenge 2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ console.log('%c HI', 'color: firebrick')
 
 document.addEventListener("DOMContentLoaded", event => {
     randomDogImages();
+    dogBreedList();
 })
 
 let randomDogImages = () => {
@@ -9,10 +10,33 @@ let randomDogImages = () => {
     .then(response => response.json())
     .then(json => {
         let imageContainer = document.getElementById("dog-image-container");
-        for(imageURL of json.message) {
+        for (imageURL of json.message) {
             let img = document.createElement("img");
             img.src = imageURL;
             imageContainer.appendChild(img);
         }
     })
+}
+
+let dogBreedList = () => {
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(json => {
+        let breedList = document.getElementById("dog-breeds");
+        for (breed in json.message) {
+            if (json.message[breed].length === 0) {
+                breedList.appendChild(getListItem(breed));
+            } else {
+                for (breedPrefix of json.message[breed]) {
+                    breedList.appendChild(getListItem(`${breedPrefix} ${breed}`))
+                }
+            }
+        }
+    })
+}
+
+let getListItem = string => {
+    let li = document.createElement('li');
+    li.innerText = string;
+    return li;
 }
